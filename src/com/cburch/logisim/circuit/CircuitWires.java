dbe91ab75fc86a565b4c306c316dbfcae9b69fc1
JarrettBillingsley <jarrett.billingsley@gmail.com>
2020-03-07 01:38:54 -0500

    think that was just a bug

--- src/com/cburch/logisim/circuit/CircuitWires.java
+++ src/com/cburch/logisim/circuit/CircuitWires.java
@@ -568,7 +568,7 @@ class CircuitWires {
 				Location s = w.e0;
 				Location t = w.e1;
 				WireBundle wb = bmap.getBundleAt(s);
-				if (!wb.isValid()) {
+				if (wb != null && !wb.isValid()) {
 					g.setColor(Value.WIDTH_ERROR_COLOR);
 				} else if (showState) {
 					if (!isValid) g.setColor(Value.NIL_COLOR);
